8acd3bf692aaae30ac90245f66b46ce57b52d5d6
Jonas Villacis <jonasvillacis@yahoo.com>
2023-05-01 09:42:54 -0500

    styles improved

--- src/app/components/modal.js
+++ src/app/components/modal.js
@@ -1,6 +1,13 @@
-export function Modal(message){
-    return(
-        <h1>Hello</h1>
-    )
+import React from "react";
 
-}
+function Modal ({ isVisible, message }){
+//   if (!isVisible) return null;
+
+  return (
+    <div className="fixed inset 0 bg-white bg-opacity-25 background-blur-sm flex justify-center items-center">
+      Modal
+    </div>
+  );
+};
+
+export default Modal;
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React, { Fragment } from "react";
 import { useState } from "react";
 import Image from "next/image";
 import { Alert } from "./components/alert";
-import { Modal } from "./components/modal";
+import Modal from "./components/modal";
 
 function HomePage() {
   const [file, setFile] = useState(null);
@@ -39,34 +39,39 @@ function HomePage() {
 
   return (
     <Fragment>
-      <div className="flex h-screen justify-center content-center">
-        <div className="card w-96 h-fit bg-base-100 shadow-xl">
-          <div className="flex card-body justify-center">
-            <h1 className="card-title">File Uploader</h1>
-            <form onSubmit={handleOnSubmit}>
+      <div className="flex h-screen justify-center items-center">
+        <div className="card w-96 h-fit bg-base-100 shadow-xl place-content-center">
+          <figure>
+            {file && (
+              <Image
+                src={URL.createObjectURL(file)}
+                alt="upload file"
+                className="w-screen"
+                width={256}
+                height={256}
+              />
+            )}
+          </figure>
+          <div className="h-fit flex flex-col items-center justify-center">
+            <h1 className="card-title m-3">File Uploader</h1>
+            <form
+              className="flex flex-col items-center space-x-10"
+              onSubmit={handleOnSubmit}
+            >
               <input
                 type="file"
                 className="file-input file-input-bordered file-input-primary w-full max-w-xs"
                 onChange={handleFileChange}
               />
-              <div className="card-actions">
-                <button type="submit" className="btn">
+              <div className="card-actions m-3">
+                <button type="submit" className="btn space-y-10">
                   Upload
                 </button>
               </div>
             </form>
-            {file && (
-              <Image
-                src={URL.createObjectURL(file)}
-                alt="upload file"
-                width={256}
-                height={256}
-              />
-            )}
           </div>
         </div>
       </div>
-      <Modal/>
     </Fragment>
   );
 }
